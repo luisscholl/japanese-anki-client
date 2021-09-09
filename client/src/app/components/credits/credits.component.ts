b09db189051b8909706789f4c72b67b73c373bf4
Luis Scholl <mail@luisscholl.de>
2021-09-09 17:58:50 +0200

    generate credits of all folders

--- client/src/app/components/credits/credits.component.ts
+++ client/src/app/components/credits/credits.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import * as COPYING from 'raw-loader!./../../../../../licenses/COPYING.txt';
 import * as LGPL from 'raw-loader!./../../../../../licenses/LGPL.txt';
-import * as npmLicenses from 'raw-loader!./../../../../../licenses/npm-licenses.txt';
+import * as clientLicenses from 'raw-loader!./../../../../../licenses/client-licenses.txt';
+import * as backendLicenses from 'raw-loader!./../../../../../licenses/backend-licenses.txt';
+import * as authLicenses from 'raw-loader!./../../../../../licenses/auth-licenses.txt';
+import * as genericLicenses from 'raw-loader!./../../../../../licenses/generic-licenses.txt';
 import * as APL from 'raw-loader!./../../../../../licenses/APL/english/ARPHICPL.txt';
 
 @Component({
@@ -13,7 +16,10 @@ export class CreditsComponent implements OnInit {
 
   COPYING = (COPYING as any).default;
   LGPL = (LGPL as any).default;
-  npmLicenses = (npmLicenses as any).default;
+  clientLicenses = (clientLicenses as any).default;
+  backendLicenses = (backendLicenses as any).default;
+  authLicenses = (authLicenses as any).default;
+  genericLicenses = (genericLicenses as any).default;
   APL = (APL as any).default;
 
   constructor() { }
